217fa7225f259786c89b612c5f394c7c7e49145b
arafatGithubCode <md489288@gmail.com>
2024-05-18 07:06:30 +0600

    create sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import { useState } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
+import SidebarArea from "./scenes/global/Sidebar";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SidebarArea isSidebar={isSidebar} />
           <main className="content">
-            <Topbar />
+            <Topbar setIsSidebar={setIsSidebar} />
           </main>
         </div>
       </ThemeProvider>
